e2b4078326a553451a97d62dbdc0d59d730ccc55
Konstantin Akshentsev <akshentsev69@gmail.com>
2023-03-31 15:21:35 +0300

    Done

--- src/main/java/ru/akshentsev/Port.java
+++ src/main/java/ru/akshentsev/Port.java
@@ -1,13 +1,60 @@
 package ru.akshentsev;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Port {
+    public static void main( String[] args) {
+        String[] indexes = {"1,3-5", "2", "3-4"};
+        List<List<Integer>> sequences = convertIndexesToSequences(indexes);
+        Set<List<Integer>> uniqueGroups = getAllUniqueOrderedGroups(sequences);
+
+        uniqueGroups.forEach(group -> System.out.println(group));
+    }
+
+    public static List<List<Integer>> convertIndexesToSequences(String[] indexes) {
+        return Arrays.stream(indexes)
+                .map(Port::convertStringToSequence)
+                .collect(Collectors.toList());
+    }
+
+    public static Set<List<Integer>> getAllUniqueOrderedGroups(List<List<Integer>> sequences) {
+        Set<List<Integer>> uniqueGroups = new LinkedHashSet<>();
+        generateOrderedGroups(uniqueGroups, sequences, 0, new ArrayList<>());
+        return uniqueGroups;
+    }
+
+    private static List<Integer> convertStringToSequence(String index) {
+        List<Integer> sequence = new ArrayList<>();
+        String[] parts = index.split(",");
+
+        for (String part : parts) {
+            if (part.contains("-")) {
+                String[] range = part.split("-");
+                int start = Integer.parseInt(range[0]);
+                int end = Integer.parseInt(range[1]);
+
+                for (int i = start; i <= end; i++) {
+                    sequence.add(i);
+                }
+            } else {
+                sequence.add(Integer.parseInt(part));
+            }
+        }
+
+        return sequence;
+    }
+
+    private static void generateOrderedGroups(Set<List<Integer>> uniqueGroups, List<List<Integer>> sequences, int depth, List<Integer> currentGroup) {
+        if (depth == sequences.size()) {
+            uniqueGroups.add(new ArrayList<>(currentGroup));
+            return;
+        }
+
+        for (Integer value : sequences.get(depth)) {
+            currentGroup.add(value);
+            generateOrderedGroups(uniqueGroups, sequences, depth + 1, currentGroup);
+            currentGroup.remove(currentGroup.size() - 1);
+        }
     }
 }
--- src/test/java/ru/akshentsev/PortTest.java
+++ src/test/java/ru/akshentsev/PortTest.java
@@ -1,38 +1,45 @@
 package ru.akshentsev;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+public class PortTest {
+
+    @Test
+    public void testConvertIndexesToSequences() {
+        String[] indexes = {"1,3-5", "2", "3-4"};
+        List<List<Integer>> expected = Arrays.asList(
+                Arrays.asList(1, 3, 4, 5),
+                Arrays.asList(2),
+                Arrays.asList(3, 4)
+        );
+        List<List<Integer>> actual = Port.convertIndexesToSequences(indexes);
+        assertEquals(expected, actual);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testGetAllUniqueOrderedGroups() {
+        List<List<Integer>> sequences = Arrays.asList(
+                Arrays.asList(1, 3, 4, 5),
+                Arrays.asList(2),
+                Arrays.asList(3, 4)
+        );
+        Set<List<Integer>> expected = Set.of(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(1, 2, 4),
+                Arrays.asList(3, 2, 3),
+                Arrays.asList(3, 2, 4),
+                Arrays.asList(4, 2, 3),
+                Arrays.asList(4, 2, 4),
+                Arrays.asList(5, 2, 3),
+                Arrays.asList(5, 2, 4)
+        );
+        Set<List<Integer>> actual = Port.getAllUniqueOrderedGroups(sequences);
+        assertEquals(expected, actual);
     }
 }
